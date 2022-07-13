c41d80436d10dec04a22907b9df2e35ef07de246
Leo xiao <leo.xiao@oocl.com>
2022-07-13 16:02:34 +0800

    add answer

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,8 +13,32 @@ public class App {
     }
 
     public String bestCharge(List<String> inputs) {
-        //TODO: write code here
-
+        //TODO: write code herepackage com.company;
+        //        HashMap<String, String> re=new HashMap<>();
+        //        HashMap<String, Integer> price=new HashMap<>();
+        //        re.put("ITEM0001","Braised chicken");
+        //        re.put("ITEM0013","Chinese hamburger");
+        //        re.put("ITEM0022","Cold noodles");
+        //        price.put("ITEM0001",18);
+        //        price.put("ITEM0013",6);
+        //        price.put("ITEM0022",8);
+        //        int sum1=0;
+        //        int sum2=0;
+        //        System.out.println("============= Order details =============");
+        //        for (int i=0;i<items.length;i++) {
+        //            System.out.println(re.get(items[i].substring(0,6))+"*"+items[i].substring(11)+"="+(price.get(items[i].substring(0,6))*Integer.parseInt((items[i].substring(11)))));
+        //            if(items[i].substring(0,6).equals("ITEM0001") || items[i].substring(0,6).equals("ITEM00022"))
+        //                sum1+=(Integer.parseInt((items[i].substring(11)))/2)*price.get(items[i].substring(0,6));
+        //        }
+        //        for (int j=0;j<items.length;j++) {
+        //                sum2+=(Integer.parseInt((items[j].substring(11))))*price.get(items[j].substring(0,6));
+        //        }
+        //        if (sum2>=30){
+        //            sum2-=6;
+        //        }
+        //        System.out.println("");
+        //    }
+        //}
         return null;
     }
 }
